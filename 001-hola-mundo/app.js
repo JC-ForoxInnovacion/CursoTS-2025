457f692dfbf63f46ed638b4f4e3c6b1f66088c01
JC <jc.foroxinnovacion@gmail.com>
2025-04-23 12:50:29 +0200

    Add 001-hola-mundo

---
+++ 001-hola-mundo/app.js
@@ -0,0 +1,6 @@
+"use strict";
+// import { sepCorto, sepLargo } from "./separadores.js";
+const SALUDO = "Hola mundo desde la constante";
+let saludo = "Hola mundo desde la variable";
+console.log(SALUDO);
+console.log(saludo);
---
+++ 001-hola-mundo/app.ts
@@ -0,0 +1,8 @@
+// import { sepCorto, sepLargo } from "./separadores.js";
+
+const SALUDO: string = "Hola mundo desde la constante"
+
+let saludo: string = "Hola mundo desde la variable"
+
+console.log(SALUDO);
+console.log(saludo);
