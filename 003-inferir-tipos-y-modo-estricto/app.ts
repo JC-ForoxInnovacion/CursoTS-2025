43e869d244020f5184f3f4f6a97c5cd582764389
JC <jc.foroxinnovacion@gmail.com>
2025-04-23 14:27:10 +0200

    Add 003-inferir-tipos-y-modo-estricto

---
+++ 003-inferir-tipos-y-modo-estricto/app.ts
@@ -0,0 +1,33 @@
+// import { sepCorto, sepLargo } from "./separadores.js";
+
+const K = 5;        // Se infiere el tipo 
+let nombre = "JC"   // Se infiere el tipo 
+
+const L: number = 10; // Se especifica el tipo
+let num: number = 77; // Se especifica el tipo
+
+/**
+ * 200 líneas más abajo vas a agradecer la ayuda de
+ * VSCode con el tipado en TypeScript
+ * 
+ * Te recordarás de esto 200 líneas más abajo cuando
+ * le dejes el ratón encima de algo que hayas tipado :)
+ */
+
+
+// any
+
+/**
+ * Cuando solo inicializas la variable sin tiparla o 
+ * no especificas el tipo en los parámetros de una 
+ * función ambas aceptarán cualquier tipo de dato 
+ * válido  (any)
+*/
+let c;
+
+
+function myFunction(param) {
+  console.log(param);
+}
+
+myFunction("Hola")
