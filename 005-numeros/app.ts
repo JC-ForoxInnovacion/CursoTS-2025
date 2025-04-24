e0f006e5383b618911acac67b0e4acd98b97ee21
JC <jc.foroxinnovacion@gmail.com>
2025-04-24 09:31:56 +0200

    Add 005-numeros

---
+++ 005-numeros/app.ts
@@ -0,0 +1,54 @@
+// import { sepCorto, sepLargo } from "./separadores.js";
+
+let verdes: number;
+let rojos: number = 7;
+
+/**
+ * Al no especificar el tipo de verdes  vamos a obtener este error
+ *  'verdes' is possibly 'undefined'.ts(18048)
+ *  let verdes: undefined
+ */
+
+verdes = 11;
+// verdes = "10";
+// verdes = "Hola"; // string vacío = false en cualquier otro
+                    // caso es true (no importa el tamaño del
+                    // string)
+// verdes = "Hola a todos los integrantes del equipo verde";
+
+
+if (verdes > rojos) {
+  console.log("verdes > rojos");
+} else if (verdes === rojos) {
+  console.log("verdes = rojos");
+} else {
+  console.log("verdes < rojos");
+}
+
+
+
+/**
+ * Casting
+ *  
+ * NaN (not a number) se considera un número pero no posee
+ * un valor computable válido para un número.
+ */
+
+let personas: number = 0;
+
+console.log({personas});
+
+personas = Number(true);
+console.log({personas});
+
+personas = Number(false);
+console.log({personas});
+
+personas = Number("Hola");
+console.log({personas});
+
+personas = Number(5 + 5);
+console.log({personas});
+
+personas = Number(5 + "5A");
+console.log({personas});
