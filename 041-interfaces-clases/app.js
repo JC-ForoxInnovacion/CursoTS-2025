53b555dadde730346fdf76dc5798c16ec94037c3
JC <jc.foroxinnovacion@gmail.com>
2025-05-21 12:35:25 +0200

    Update 041-interfaces-clases

--- 041-interfaces-clases/app.js
+++ 041-interfaces-clases/app.js
@@ -1,13 +1,16 @@
 (() => {
     class Persona {
-        constructor(nombre, apellidos, profesion) {
-            this.nombre = nombre;
-            this.apellidos = apellidos;
-            this.profesion = profesion;
-            console.log(nombre, apellidos, profesion);
+        constructor(nomb, apel, prof) {
+            this.nombre = nomb;
+            this.apellidos = apel;
+            this.profesion = prof;
+        }
+        getProfesion() {
+            console.log(`${this.nombre} ${this.apellidos} es ${this.profesion}`);
         }
     }
     const jc = new Persona('JC', 'VI', 'Ingeniero');
+    jc.getProfesion();
 })();
 export {};
 //# sourceMappingURL=app.js.map
--- 041-interfaces-clases/app.ts
+++ 041-interfaces-clases/app.ts
@@ -6,40 +6,68 @@ import { sepCorto, sepLargo } from "./separadores.js";
 interface IPersona {
   nombre: string,
   apellidos: string,
-  profesion: string
+  profesion: string,
 }
 
-class Persona implements IPersona {
+interface IProfesion {
+  getProfesion(): void,
+}
 
-  constructor(
-      public nombre: string,
-      public apellidos: string,
-      public profesion: string
-  ) {
 
-    console.log(nombre, apellidos, profesion);
+// Puedes implementar una o más interfaces
+class Persona implements IPersona, IProfesion {
+
+  
+  // Método 1: Constructor con propiedades de
+  // la interface
+
+  /**
+   * Para eliminar el error de implementación de
+   * la interface Persona las propiedades en el
+   * constructor deben levar 'public' explícitamete
+   */
+  
+  
+  // constructor(
+  //     public nombre: string,
+  //     public apellidos: string,
+  //     public profesion: string
+  // ) {
+
+  //   console.log(nombre, apellidos, profesion);
+  // }
+
+
+
+  // Método 2: Declaración explícita de las 
+  // propiedades establecidas en la interface 
+  // como propiedades de clase 
+
+
+  nombre: string;
+  apellidos: string;
+  profesion: string;
+  
+  constructor( nomb: string, apel: string, prof: string) {
+    this.nombre = nomb;
+    this.apellidos = apel;
+    this.profesion = prof;
+
+
+    // console.log(nomb, apel, prof); // parámetros
+    
+    
+    // console.log(this.nombre, this.apellidos, this.profesion); // propiedades de clase
   }
 
-
-  // nombre: string;
-  // apellidos: string;
-  // profesion: string;
-  
-  // constructor( nomb: string, apel: string, prof: string) {
-  //   this.nombre = nomb;
-  //   this.apellidos = apel;
-  //   this.profesion = prof;
-
-
-  //   console.log(nomb, apel, prof); // parámetros
-    
-    
-  //   console.log(this.nombre, this.apellidos, this.profesion); // propiedades de clase
-  // }
+  getProfesion(): void {
+    console.log(`${this.nombre} ${this.apellidos} es ${this.profesion}` );
+  } 
 
 }
 
 
 const jc = new Persona('JC', 'VI', 'Ingeniero')
+jc.getProfesion();
 })();
 
